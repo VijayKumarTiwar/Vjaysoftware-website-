a328c27c3da7c3e306516c69ed976c8a1b3e11f6
Vijay Kumar Tiwari <vijaykumartiwar@users.noreply.github.com>
2026-05-19 15:10:28 +0530

    Update allowedDevOrigins in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.1.36'],
+  allowedDevOrigins: ['192.168.1.36', '192.168.1.42'],
 };
 
 export default nextConfig;
